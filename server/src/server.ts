b473e03082e0888b45ebd1a2d1d8a0ae3b220c0f
Pedro Hamms Vietro <hammsvietro@gmail.com>
2020-07-01 16:21:17 -0400

    created the .example.env enviroment variables boilerplate when changing enviroment

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,4 +4,7 @@ import app from './app';
 
 dotenv.config();
 
-app.listen(process.env.SV_PORT, () => process.env.SV_ADDRESS);
+app.listen(process.env.SV_PORT, () => {
+  console.log(`listening at: ${process.env.SV_ADDRESS}:${process.env.SV_PORT}`);
+  return process.env.SV_PORT;
+});
